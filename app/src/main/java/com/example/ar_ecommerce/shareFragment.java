643305fc89b028501125227a7505b955f750678c
keer <keerthananayak82@gmail.com>
2023-04-09 18:28:23 +0530

    feat:navigation drawer implemented

---
+++ app/src/main/java/com/example/ar_ecommerce/shareFragment.java
@@ -0,0 +1,22 @@
+package com.example.ar_ecommerce;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class shareFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_share, container, false);
+    }
+}
